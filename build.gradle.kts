767716fb50d803ef5d6c5dae6703bf4dfcf2e75d
Raúl Rodríguez Concepción <raulrcs@gmail.com>
2021-02-15 23:44:05 +0100

    migrate to kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+
+    compileSdkVersion(Dependencies.SdkVersion.sdkVersion)
+    buildToolsVersion(Dependencies.SdkVersion.buildVersion)
+
+    defaultConfig {
+        minSdkVersion(Dependencies.SdkVersion.minSdkVersion)
+        targetSdkVersion(Dependencies.SdkVersion.sdkVersion)
+        versionCode = 1
+        versionName = "1.0"
+        // consumerProguardFiles = "consumer-rules.pro"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isDebuggable = false
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    lintOptions {
+        isCheckReleaseBuilds = false
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+}
+
+dependencies {
+    implementation(Dependencies.Kotlin.stdlib)
+    implementation(Dependencies.Android.appcompat)
+    implementation(Dependencies.Android.material)
+    implementation(Dependencies.Android.preference)
+
+}
